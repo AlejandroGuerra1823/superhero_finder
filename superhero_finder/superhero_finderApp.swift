a5b0000d611ed5d3daa4a810f1ec3b0e2e69e59b
Alejandro Guerra <guerramanuel299@gmail.com>
2024-08-24 17:51:17 -0500

    Initial Commit

---
+++ superhero_finder/superhero_finderApp.swift
@@ -0,0 +1,17 @@
+//
+//  superhero_finderApp.swift
+//  superhero_finder
+//
+//  Created by Alejandro Guerra on 24/08/24.
+//
+
+import SwiftUI
+
+@main
+struct superhero_finderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
